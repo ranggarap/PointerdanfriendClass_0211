e4a25f26853071415b64046ce002956c30daf493
ranggarap <fadhilahrangga26@gmail.com>
2025-05-05 18:39:39 +0700

    membuat dereference

---
+++ pointer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+ //dereference
+ int a =10;
+ int b = 6;
+ int y = 20;
+ int *c= &a;
+ cout << "Alamat memori:" << c << endl;
+ cout << "nilai dari c :" << *c <<endl;
+ c= &y;
+ cout << "Alamat memori :" << c<< endl;
+ cout << "Nilai dari C :" << *c<< endl;
+
+
+}
